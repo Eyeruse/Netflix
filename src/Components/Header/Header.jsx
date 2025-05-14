3c7e4a4cd1f56a2655cb6092d06c468896dc4608
Eyeruse <eyerusalemg11@gmail.com>
2025-05-13 23:45:53 -0400

    Added Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,2 +1,53 @@
 import React from "react";
-import "./Header.css"
+import NetflixLogo from "../../assets/images/netflix.png"
+import "./Header.css";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoMdNotificationsOutline, IpMdNotificationiconsOutline } from "react-icons/io";
+import { MdOutlineAccountBox } from "react-icons/md";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+
+function Header() {
+  return (
+    <div>
+      <div className="header-outer-container">
+        <div className="header-container">
+          <div className="header-left">
+            <ul>
+              <li>
+                <img src={NetflixLogo} alt="Netflix logo" />
+              </li>
+              <li>Netflix</li>
+              <li>Home</li>
+              <li>TVShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>My List</li>
+              <li>Browse by Languages</li>
+            </ul>
+          </div>
+          <div className="header-right">
+          <ul>
+            <li>
+              <IoSearchOutline />
+            </li>
+            <li>
+              <IoMdNotificationsOutline />
+            </li>
+            <li>
+              <MdOutlineAccountBox />
+            </li>
+            <li>
+              <IoMdArrowDropdown />
+            </li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
+
+
