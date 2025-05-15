214ae9b036230fe8a06b1c2c7f5ef9d896784d08
Eyeruse <eyerusalemg11@gmail.com>
2025-05-14 22:56:18 -0400

    Footer added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
 
 function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
