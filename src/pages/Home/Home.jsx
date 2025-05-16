6415569de5349434559295296386d785c818d56e
Eyeruse <eyerusalemg11@gmail.com>
2025-05-15 22:34:32 -0400

    Banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/banner'
+import RowList from '../../components/Rows/RowList/RowList';
 
 
 function Home() {
   return (
     <div>
       <Header />
+      <Banner />
+      <RowList />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default Home 
